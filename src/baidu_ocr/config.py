c9495613b501ff1d6c5951d4d08e50eb84555e09
buptlihang <18801481780@163.com>
2020-08-19 07:11:16 +0000

    update test

---
+++ src/baidu_ocr/config.py
@@ -0,0 +1,8 @@
+from syszux_config import *
+config.test = AttrDict()
+
+config.test.APP_ID = ''
+config.test.API_KEY = ''
+config.test.SECRET_KEY = ''
+config.test.test_file = '../../dataset/ocr_on_news.txt'
+config.test.test_dataset = '../../dataset/ocr_on_news'
